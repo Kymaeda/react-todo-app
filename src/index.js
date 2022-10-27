8c402a73cbd0731c19c7aace553dae6f07784df2
Kymaeda <49601393+Kymaeda@users.noreply.github.com>
2022-10-28 08:28:23 +0900

    コンポーネント化

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-  return (
-    // vuejsの、templateと同じようなタグ. <>だけでも良い
-    <React.Fragment>
-      <h1>こんにちは！！！</h1>
-      <p>hi</p>
-    </React.Fragment>
-  );
-};
+import App from "./app";
 
 ReactDom.render(<App />, document.getElementById("root"));
