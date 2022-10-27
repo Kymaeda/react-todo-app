c86c1ce48f47f04325a3294dfcefd438a1efb53f
Kymaeda <49601393+Kymaeda@users.noreply.github.com>
2022-10-27 09:15:10 +0900

    jsxの記法

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
-import ReactDom from "rect-dom";
+import ReactDom from "react-dom";
 
 const App = () => {
-  return null;
+  return (
+    // vuejsの、templateと同じようなタグ. <>だけでも良い
+    <React.Fragment>
+      <h1>こんにちは！！！</h1>
+      <p>hi</p>
+    </React.Fragment>
+  );
 };
 
 ReactDom.render(<App />, document.getElementById("root"));
