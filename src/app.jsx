e8d753fb987c26433cd62de85bccadcee087af32
Kymaeda <49601393+Kymaeda@users.noreply.github.com>
2022-10-28 08:33:09 +0900

    イベントハンドリングとスタイリング

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 const App = () => {
+  const onClick = () => {
+    alert("clicked");
+  };
+  const styles = {
+    color: "red", // オブジェクトなので、文字列で指定する
+    fontSize: "18px" // キャメルケースで書く
+  };
   return (
     <>
-      <h1>こんにちは！！！</h1>
+      <h1 style={styles}>こんにちは！！！</h1>
       <p>hi</p>
+      <button onClick={onClick}>ボタン</button>
     </>
   );
 };
