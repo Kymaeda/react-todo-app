b16eee584132e892eafaa6d4a9e986ae5b18e961
Kymaeda <49601393+Kymaeda@users.noreply.github.com>
2022-10-29 08:59:23 +0900

    useStateを使う https://ja.reactjs.org/docs/hooks-state.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ColoredMessage from "./components/ColoredMessage";
 
 const App = () => {
-  const onClick = () => {
-    alert("clicked");
+  const countUp = () => {
+    setNum(num + 1);
   };
   const styles = {
     color: "red", // オブジェクトなので、文字列で指定する
     fontSize: "18px" // キャメルケースで書く
   };
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={styles}>こんにちは！！！</h1>
       <ColoredMessage color="green">お元気ですか？？</ColoredMessage>
       <ColoredMessage color="blue">ありがとう、元気です！</ColoredMessage>
-      <button onClick={onClick}>ボタン</button>
+      <button onClick={countUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
