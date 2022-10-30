8fef6a981edc62b48be60cc5eb8a0b460b01d6d4
Kymaeda <49601393+Kymaeda@users.noreply.github.com>
2022-10-31 08:47:20 +0900

    HTML/CSSコーディング

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
 import React from "react";
+import "./styles.css";
 
 export const App = () => {
-  return <div>HELLO</div>;
+  return (
+    <>
+      <div className="area input-area">
+        <input placeholder="TODOを入力" id="input" />
+        <button id="add">追加</button>
+      </div>
+
+      <div className="area incompleted-area">
+        <p className="title">未完了のTODO</p>
+        <ul id="incompletion-list">
+          <li>
+            <div className="list-row">
+              <p>test</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div className="list-row">
+              <p>test</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="area completed-area">
+        <p className="title">完了のTODO</p>
+        <ul id="completed-list">
+          <li>
+            <div className="list-row">
+              <p>test</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
