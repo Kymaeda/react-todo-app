2e11bc2bd7cebf93c501a2ada5c52d7e358b9661
Kymaeda <49601393+Kymaeda@users.noreply.github.com>
2022-11-01 08:15:56 +0900

    mapを用いてloop処理を実装する

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompletedTodo, setIncompletedTodo] = useState(["aaaa", "bbb"]);
+  const [completedTodo, setCompletedTodo] = useState(["ccc", "ddd"]);
+
   return (
     <>
       <div className="area input-area">
@@ -12,31 +15,33 @@ export const App = () => {
       <div className="area incompleted-area">
         <p className="title">未完了のTODO</p>
         <ul id="incompletion-list">
-          <li>
-            <div className="list-row">
-              <p>test</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
-          <li>
-            <div className="list-row">
-              <p>test</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {incompletedTodo.map((todo) => {
+            return (
+              // NOTE: loop処理の際はkeyを設定する(部分レンダリングのため)
+              <li key={todo}>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="area completed-area">
         <p className="title">完了のTODO</p>
         <ul id="completed-list">
-          <li>
-            <div className="list-row">
-              <p>test</p>
-              <button>戻す</button>
-            </div>
-          </li>
+          {completedTodo.map((todo) => {
+            return (
+              <li key={todo}>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button>戻す</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
