927ca8a4b2d80ef6e10a5ba946f78527f22ba4e1
Kymaeda <49601393+Kymaeda@users.noreply.github.com>
2022-11-03 08:47:23 +0900

    タスクの削除機能実装

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,15 @@ export const App = () => {
   const updateTodoText = (e) => setTodoText(e.target.value);
   const addTodo = () => {
     if (todoText === "") return;
+    // NOTE: incompletedTodoをimuutableなものとして扱う(good)
     const newTodos = [...incompletedTodo, todoText];
     setIncompletedTodo(newTodos);
     setTodoText("");
+  };
+  const removeTodo = (index) => {
+    const newTodos = [...incompletedTodo];
+    newTodos.splice(index, 1);
+    setIncompletedTodo(newTodos);
   };
   return (
     <>
@@ -31,14 +37,18 @@ export const App = () => {
       <div className="area incompleted-area">
         <p className="title">未完了のTODO</p>
         <ul id="incompletion-list">
-          {incompletedTodo.map((todo) => {
+          {incompletedTodo.map((todo, index) => {
             return (
               // NOTE: loop処理の際はkeyを設定する(部分レンダリングのため)
               <li key={todo}>
                 <div className="list-row">
                   <p>{todo}</p>
                   <button>完了</button>
-                  <button>削除</button>
+                  {/*
+                    関数に引数を渡したい場合はarrow関数で新たに呼び出す
+                    {removeTodo(index)}だと、ページ描画時に実行されてしまう
+                  */}
+                  <button onClick={() => removeTodo(index)}>削除</button>
                 </div>
               </li>
             );
