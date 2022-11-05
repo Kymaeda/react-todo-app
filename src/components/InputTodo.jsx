b2739dca66c8f6e05d42478801af2c6dbab372cb
Kymaeda <49601393+Kymaeda@users.noreply.github.com>
2022-11-05 10:28:23 +0900

    css in js

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  height: "30px",
+  width: "400px",
+  padding: "10px",
+  margin: "10px",
+  borderRadius: "5px"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="area input-area">
+    <div style={style}>
       {/* setTodoTextした時に連動させたいからvalueプロパティも必要 */}
       <input
         placeholder="TODOを入力"
